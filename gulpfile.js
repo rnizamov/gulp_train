96c5904e22b84d9ad8213863d43d5520a931a169
Nizamov Rinat <rnizamov@ovapress.ru>
2017-08-28 17:14:51 +0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,21 @@ const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
+const newer = require('gulp-newer');
+const browserSync = require('browser-sync').create();
+const notify = require('gulp-notify');
+const multipipe = require('multipipe');
+
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development'; 
 
 gulp.task('styles',function() {
-	return gulp.src('frontend/styles/main.styl')
-		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
-		.pipe(stylus())
-		.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
-		.pipe(gulp.dest('public'));
+	return multipipe ( 
+		gulp.src('frontend/styles/main.styl'),
+		gulpIf(isDevelopment, sourcemaps.init()),
+		stylus(),
+ 		gulpIf(isDevelopment, sourcemaps.write()),
+		gulp.dest('public')
+	).on('error', notify.onError());
 });
 
 gulp.task('clean', function() {
@@ -21,18 +28,25 @@ gulp.task('clean', function() {
 
 gulp.task('assets',function() {
 	return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
+		.pipe(newer('public'))
 		.pipe(debug({title: 'assets'}))
 		.pipe(gulp.dest('public'));
 });
 
-gulp.task('build',gulp.series(
-	'clean',
-	gulp.parallel('styles','assets'))
-);
+gulp.task('build',gulp.series('clean',gulp.parallel('styles','assets')));
 
 gulp.task('watch',function() {
 	gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
 	gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
 });
 
-gulp.task('dev',gulp.series('build','watch'));
+
+gulp.task('serve',function() {
+	browserSync.init({
+		server: 'public'
+	});
+
+	browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev',gulp.series('build', gulp.parallel('watch','serve')));
